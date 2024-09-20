e97d11867c7f7b037c556cd6698d22f1801093a3
salma <salma.a.samei@gmail.com>
2024-09-21 02:04:43 +0300

    first commit

---
+++ task.js
@@ -0,0 +1,13 @@
+let zero=0;
+let counter =3;
+let arr=['kiro','eslam','sayed','tarek','ali','marwan']
+arr.pop();
+arr.pop();
+arr.reverse();
+console.log(arr);//tarek sayed eslam kero
+zero++;
+console.log(arr.slice(zero++,counter))
+// console.log(arr)
+console.log(arr[zero--][zero--]+arr[zero].slice(zero,counter))
+
+
